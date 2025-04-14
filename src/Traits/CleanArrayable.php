054d2d0169daa67630e9139e41fd0653b16b0525
chermant <chermant@amphibee.fr>
2025-04-14 12:02:07 +0200

    fix: remove dto empty value on toArray method

--- src/Traits/CleanArrayable.php
+++ src/Traits/CleanArrayable.php
@@ -7,7 +7,7 @@ trait CleanArrayable
     public function toArray(): array
     {
         return array_filter(get_object_vars($this), function ($value) {
-            return $value !== null;
+            return $value !== null || !empty($valeur);
         });
     }
 } 
